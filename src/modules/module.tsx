0af1cbbd8ecfa6b5f16ea8ef8b0fa99b06d56dc7
SidLD <cidlibril5@gmail.com>
2024-12-06 09:15:35 +0800

    fs

---
+++ src/modules/module.tsx
@@ -0,0 +1,21 @@
+
+
+import { auth } from '@/lib/services';
+import { Navigate, Outlet } from "react-router-dom"
+
+export const PublicLayout = () => {
+    
+    if(auth.isAuthenticated()){
+        return  <Navigate to={'/user'} />;
+    }
+    return  <Navigate to={'/'} />;
+}
+
+export const PrivateLayout = () => {
+
+    if (!auth.isAuthenticated()) {
+        return <Navigate to={"/"} />;
+    }
+    return <>
+    <Outlet /></> 
+}
